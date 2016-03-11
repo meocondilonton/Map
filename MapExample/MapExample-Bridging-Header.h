6b170e1b4cdcb35bcb7306c8fdfbf6edd3d27e0d
philong8390 <philong8390@gmail.com>
2016-03-11 07:31:57 +0700

    cm

--- MapExample/MapExample-Bridging-Header.h
+++ MapExample/MapExample-Bridging-Header.h
@@ -2,18 +2,12 @@
 //  MapExample-Bridging-Header.h
 //  MapExample
 //
-//  Created by long nguyen on 04/03/2016.
+//  Created by long nguyen on 10/03/2016.
 //  Copyright © 2016 long nguyen. All rights reserved.
 //
 
 #ifndef MapExample_Bridging_Header_h
 #define MapExample_Bridging_Header_h
 
-#import <BlocksKit/BlocksKit+UIKit.h>
-#import <Masonry/Masonry.h> 
-#import <GoogleMaps/GoogleMaps.h>
-#import "AutoCompleteObject.h"
-#import "GMDraggableMarkerManager.h"
-#import "GMDraggableMarkerManagerDelegate.h"
 
 #endif /* MapExample_Bridging_Header_h */
